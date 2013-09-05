e53b21653fbfbcaa62f774f57e29c371ee28ea88
Markus Martin <markus@archwyrm.net>
2013-09-05 18:12:41 -0400

    Divide the area under the ground plane into triangles

--- lib/world.py
+++ lib/world.py
@@ -4,9 +4,13 @@ class World:
     def __init__(self):
         self.ground = ((0,400),(200,250),(320,350),(400,300),(640,400))
         self.checked = False
-        #print_slope(self.ground)
 
     def draw(self, surf):
+        for tri in self._triangulate():
+            tri = list(tri)
+            tri.append(tri[0]) # Add final side for drawing purposes
+            pygame.draw.aalines(surf, pygame.Color(255,255,0), False, tri)
+
         pygame.draw.aalines(surf, pygame.Color(255,255,255), False, self.ground)
 
     def collide(self, player):
@@ -51,25 +55,28 @@ class World:
         a = segment[0]; b = segment[1]
         return (b[1] - a[1]) / (b[0] - a[0] * 1.0)
 
-    # http://geomalgorithms.com/a02-_lines.html#Distance-to-Ray-or-Segment
-    def dist_Point_to_Segment(point, segment):
-        pass
-        v = (segment[1][0] - segment[0][0],
-             segment[1][1] - segment[0][1])
-        w = (point[1][0] - segment[0][0],
-             point[1][1] - segment[0][1])
+    def _triangulate(self):
+        """Adds a third vertex to each segment to divide the area under
+        the ground plane into triangles. Returns a list of triangle
+        segments."""
 
-        c1 = self._dot_product(w, v)
-        if c1 <= 0:
-            return d(P, S.P0);
+        tris = []
+        segments = self._get_segments()
+        size = len(segments)
+        for i in range(size):
+            seg = segments[i]
+            x = seg[0][0] + ((seg[1][0] - seg[0][0]) / 2)
+            y = 480
+            tri = (seg[0], seg[1], (x,y))
 
-        c2 = self._dot_product(v, v)
-        if c2 <= c1:
-            return d(P, S.P1)
+            # Create first tri, unless it is in the corner
+            if i == 0 and seg[0][1] != y:
+                tris.append(((0,y), seg[0], (x,y)))
 
-        b = c1 / c2
-        Pb = S.P0 + b * v
-        return d(P, Pb)
+            tris.append(tri)
 
-    def _dot_product(self, a, b):
-        return a[0] * b[0] + a[1] * b[1]
+            # Create last tri, unless it is in the corner
+            if i == size - 1 and seg[1][1] != y:
+                tris.append((seg[1], (640,y), (x,y)))
+
+        return tris
